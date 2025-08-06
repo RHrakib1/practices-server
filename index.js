b110bce4eeedc6ae9673905e38926383b3a0017c
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-07 01:13:50 +0600

    add to the get

--- index.js
+++ index.js
@@ -6,6 +6,55 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rakibrh1232:YKk0HVkEDstk1pvl@cluster0.vlbcfwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const database = client.db("datapracticebase");
+        const data = database.collection("basebata");
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        app.get('/pruser', async (req, res) => {
+            const courser = data.find()
+            const result = await courser.toArray()
+            res.send(result)
+        })
+
+        app.post('/pruser', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const result = await data.insertOne(user);
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
     res.send('simple database oparation done')
 })
